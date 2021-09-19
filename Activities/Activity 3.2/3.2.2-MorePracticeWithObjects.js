9b3240be8b5a7d0e3cfc9337687084e36a147455
Martin Mantler <mmantler@gmail.com>
2021-09-19 15:04:04 -0500

    Activty 3.2.2 completed
    Completed Activity 3.2.2

--- Activities/Activity 3.2/3.2.2-MorePracticeWithObjects.js
+++ Activities/Activity 3.2/3.2.2-MorePracticeWithObjects.js
@@ -5,51 +5,48 @@ const obj = {
   1: console.log('the key is 1'),
 };
 // what will the following lines print?
-obj[key];
-
-obj.key;
-
-obj['key'];
-
-//Prints out:
-// the key is 'key'
-// the key is 1
-
+obj[key];     //  Output: the key is 'key'
+ 
+obj.key;      //  Output: the key is 1
+ 
+obj['key'];   //  Output: No output is produced
+ 
 // create an object with at least four properties, each with a different data type
+const myObj = {
+  aString: "hello",
+  aNumber: 99,
+  aBoolean: true,
+  acollection: {p1: "a", p2:"b",p3:"c"}
+}
 // name one of the four properties "collection" and set its value to an Array or Object
-
-let myObj = {
-  prop1: 1,
-  prop2: 'hello',
-  prop3: true,
-  collection: ['a', 'b', 'c'],
-};
+myObj.acollection = [11,22,33,44]
 
 // access a value in in "collection" property
-console.log(myObj.collection[0]);
+console.log(myObj.acollection[2])
 // Nested Arrays and Objects
 const keys = ['key', '1', 'method', 'favorites', 'list'];
-// console.log(keys);
 obj.method = function () {
-  //  return "the key is 'method' and it calls refers to a function";
-  return `the key is 'method' and it call refers to a function`;
+  return "the key is 'method' and it calls refers to a function";
 };
 // fill in your favorite movie and color below
 obj['favorites'] = {
-  movie: '007',
-  number: 14,
-  color: 'red',
+  movie: 'James Bond',
+  number: 7,
+  color: 'Blue',
 };
 obj.list = ['a', 'b', 3, 'd', ['e', 4, 5], { f: 6 }];
 
 // how many lines will the following for...of statement print?
-// It will print 5 lines
 // what do you expect to see on each line?
-//
-for (let akey of keys) {
-  console.log('key test', akey);
-  console.log(`Key is: ${akey} and value is: ${obj[akey]}`);
+for (let key in keys) {
+  console.log(`Key is: ${key} and value is: ${obj[key]}`);
 }
+//Expected output:
+    //Key is: 0 and value is: undefined
+    // Key is: 1 and value is: undefined    
+    // Key is: 2 and value is: undefined    
+    // Key is: 3 and value is: undefined    
+    // Key is: 4 and value is: undefined
 // use a template literal to print a sentence about your favorite movie and color
-
+console.log(`My favorite movies are ${obj['favorites'].movie} and my favorite color is ${obj['favorites'].color}.`)
 // access the values "b", 4, and 6 from obj.list
